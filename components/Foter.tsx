ab4a25fb4f63816288990833c788b4b32835978e
Kadacheahmedrami <a_kadache@estin.dz>
2025-01-30 14:58:13 +0100

    udnerline

--- components/Foter.tsx
+++ components/Foter.tsx
@@ -19,10 +19,10 @@ const Footer: React.FC = () => {
                     <Image src="/svg/estin.svg" alt="ESTIN Logo" width={200} height={200} className="cursor-pointer"/>
                     <p className="text-[24px] w-full">National road n° 75, Amizour 06300 Bejaia, Algeria</p>
                     <p className="text-[24px]">
-                    <a href="tel:+21334824916" className="text-[24px] text-[#F1413E] hover:underline">+213-34-824-916</a>
+                    <a href="tel:+21334824916" className="text-[24px] text-[#F1413E] underline">+213-34-824-916</a>
                     </p>
 
-                    <Link href="mailto:contact@estin.dz" className="text-[24px] text-[#F1413E] hover:underline">contact@estin.dz</Link>
+                    <Link href="mailto:contact@estin.dz" className="text-[24px] text-[#F1413E] underline">contact@estin.dz</Link>
 
                     <div className="h-full  text-start text-base border-t border-gray-700 pt-4 w-full">
                         <p>© Copyrights. All rights reserved ESTIN - Bejaia</p>
