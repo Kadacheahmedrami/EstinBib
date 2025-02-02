a912ba764cf804ccdcb0877bbc764ef451843757
islem-boudja <i_boudja@estin.dz>
2025-02-02 02:14:21 +0100

    refactor: add 'res.cloudinary.com' to allowed image domains in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images : {
-    domains : ['loremflickr.com']
-  }
+  images: {
+    domains: ["loremflickr.com", "res.cloudinary.com"],
+  },
   /* config options here */
 };
 
