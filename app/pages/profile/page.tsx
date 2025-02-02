ec18b1634b7566d39c4ef58d4fbe6a93f6bb549b
islem-boudja <i_boudja@estin.dz>
2025-02-02 03:12:54 +0100

    feat: add Signout component and update borrowed books and user info components to use new props

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -1,34 +1,34 @@
-import { redirect } from "next/navigation"
-import UserInfo from "@/components/user-info"
-import BorrowedBooks from "@/components/borrowed-books"
-import BorrowHistory from "@/components/borrow-history"
-import { getServerAuthSession } from "@/lib/auth"
+import { redirect } from "next/navigation";
+import UserInfo from "@/components/user-info";
+import BorrowedBooks from "@/components/borrowed-books";
+import BorrowHistory from "@/components/borrow-history";
+import { getServerAuthSession } from "@/lib/auth";
+import { borrowsHistory, getActiveBorrows } from "@/app/actions/user";
 
 export default async function Profile() {
-  const session = await getServerAuthSession()
+  const session = await getServerAuthSession();
 
   if (!session) {
-    redirect("/auth/login")
+    redirect("/auth/login");
   }
-
+  const borrowHistory = await borrowsHistory();
+  const activeBorrows = await getActiveBorrows();
   return (
- 
-      <div className="flex w-full flex-col">
-        <UserInfo />
-        <div className="md:col-span-2 space-y-8">
-          <div className="w-full flex justify-center items-center gap-[70px] flex-row">
-            <h3 className="font-bold text-[30px]">My Borrowed Books :</h3>
-            <div className="bg-black mt-2 h-[2px] w-[70%]"></div>
-          </div>
-          <BorrowedBooks key={1} />
-
-          <div className="w-full flex justify-center items-center gap-[70px] flex-row">
-            <h3 className="font-bold text-[30px]">My Borrow History</h3>
-            <div className="bg-black mt-2 h-[2px] w-[70%]"></div>
-          </div>
-          <BorrowHistory key={2} />
+    <div className="flex w-full flex-col">
+      <UserInfo />
+      <div className="md:col-span-2 space-y-8">
+        <div className="w-full flex justify-center items-center gap-[70px] flex-row">
+          <h3 className="font-bold text-[30px]">My Borrowed Books :</h3>
+          <div className="bg-black mt-2 h-[2px] w-[70%]"></div>
         </div>
+        <BorrowedBooks key={1} books={activeBorrows} />
+
+        <div className="w-full flex justify-center items-center gap-[70px] flex-row">
+          <h3 className="font-bold text-[30px]">My Borrow History</h3>
+          <div className="bg-black mt-2 h-[2px] w-[70%]"></div>
+        </div>
+        <BorrowHistory key={2} books={borrowHistory} />
       </div>
- 
-  )
+    </div>
+  );
 }
